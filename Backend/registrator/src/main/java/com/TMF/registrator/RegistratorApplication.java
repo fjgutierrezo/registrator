cc78eadef9d8f7e6a27bb1c7f7e6c5543dd1fe80
Francisco Javier Gutierrez Obando <franciscojavier@MacBook-Pro-de-Francisco.local>
2025-03-19 21:21:21 +0100

    Inicio TFM

---
+++ Backend/registrator/src/main/java/com/TMF/registrator/RegistratorApplication.java
@@ -0,0 +1,13 @@
+package com.TMF.registrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistratorApplication.class, args);
+	}
+
+}
---
+++ Backend/registrator/src/test/java/com/TMF/registrator/RegistratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TMF.registrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
